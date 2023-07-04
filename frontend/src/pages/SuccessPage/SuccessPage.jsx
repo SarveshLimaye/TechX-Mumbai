ee3527636471114f2892cde6e25fb07b4cd05575
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-07-04 22:59:12 +0530

    Ticketing logic fixed

--- frontend/src/pages/SuccessPage/SuccessPage.jsx
+++ frontend/src/pages/SuccessPage/SuccessPage.jsx
@@ -3,15 +3,17 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { CheckCircleIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const SuccessPage = () => {
   const { user } = useAuth0();
-  const [dbUser, setDbUser] = useState({});
-  const [event, setEvent] = useState({});
+  const [dbUser, setDbUser] = useState();
   const navigate = useNavigate();
 
-  const eventId = window.location.pathname.split("/")[2];
-  const getUser = async () => {
+  const params = useParams();
+  const eventId = params.id;
+  console.log(eventId);
+  const upodateTicket = async () => {
     const response = await fetch(
       `http://localhost:5000/api/users/${user.email}`
     );
@@ -19,54 +21,33 @@ const SuccessPage = () => {
     console.log(data);
     setDbUser(data);
 
-    if (eventId) {
-      var ind = data.attendedEvents.indexOf(eventId);
-      if (ind == -1) {
-        const res2 = await fetch(
-          `http://localhost:5000/api/users/update/${data._id}`,
-          {
-            method: "PUT",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify({
-              attendedEvents: eventId,
-            }),
-          }
-        );
-        console.log(res2);
+    const res1 = await fetch(
+      `http://localhost:5000/api/users/update/${data._id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          attendedEvents: eventId,
+        }),
       }
-    }
-  };
-
-  const getEvent = async () => {
-    const response = await fetch(`http://localhost:5000/api/events/${eventId}`);
-    const data = await response.json();
-    console.log(data);
-    setEvent(data);
-
-    var ind = data.attendees.indexOf(dbUser._id);
-    if (dbUser._id) {
-      if (ind == -1) {
-        const res2 = await fetch(
-          `http://localhost:5000/api/events/attendees/${eventId}`,
-          {
-            method: "PUT",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify({
-              attendees: dbUser._id,
-            }),
-          }
-        );
-        console.log(res2);
+    );
+    console.log(dbUser._id);
+    const res2 = await fetch(
+      `http://localhost:5000/api/events/attendees/${eventId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          attendees: dbUser._id,
+        }),
       }
-    }
-  };
-  const handleClick = async () => {
-    getUser();
-    getEvent();
+    );
+    console.log(res1);
+    console.log(res2);
     navigate("/");
   };
 
@@ -80,7 +61,7 @@ const SuccessPage = () => {
               You are registered successfully for the event! Thank you!
             </Heading>
             <Text color={"gray.500"}>
-              <button onClick={handleClick}>Back to Home Page</button>
+              <button onClick={upodateTicket}>Back to Home Page</button>
             </Text>
           </Box>
         </div>
